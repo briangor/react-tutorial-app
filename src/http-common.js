4152742b6bfe0bec5add5969b6ff046b53075934
briangor <briangor13@gmail.com>
2024-03-05 20:59:55 +0300

    update baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8082/api/",
+    baseURL: process.env.URL || "http://localhost:8082/api/",
     headers: {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "*"
