71ca21eea17b8d61d916258a76ba3df3c3a045a7
briangor <briangor13@gmail.com>
2024-03-05 22:11:49 +0300

    update the env var to react syntax

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.URL || "http://localhost:8082/api/",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8082/api/",
     headers: {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "*"
